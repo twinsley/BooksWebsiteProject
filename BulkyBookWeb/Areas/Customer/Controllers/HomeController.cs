6f76cc3897c22e97171f51121c2cbe98de0e65f0
Tim Winsley <carpentertimw@gmail.com>
2023-03-13 21:43:06 -0400

    Fix accidentally copied file

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -11,77 +11,77 @@ using System.Diagnostics;
 using System.Linq;
 using System.Security.Claims;
 
-namespace BulkyBookWeb.Controllers;
+namespace BulkyBookWeb.Areas.Customer.Controllers;
 [Area("Customer")]
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly IUnitOfWork _unitOfWork;
+	private readonly ILogger<HomeController> _logger;
+	private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-    {
-        _logger = logger;
-        _unitOfWork = unitOfWork;
-    }
+	public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+	{
+		_logger = logger;
+		_unitOfWork = unitOfWork;
+	}
 
-    public IActionResult Index()
-    {
-        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+	public IActionResult Index()
+	{
+		IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
 
-        return View(productList);
-    }
+		return View(productList);
+	}
 
-    public IActionResult Details(int productId)
-    {
-        ShoppingCart cartObj = new()
-        {
-            Count = 1,
-            ProductId = productId,
-            Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == productId, includeProperties: "Category,CoverType"),
-        };
+	public IActionResult Details(int productId)
+	{
+		ShoppingCart cartObj = new()
+		{
+			Count = 1,
+			ProductId = productId,
+			Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == productId, includeProperties: "Category,CoverType"),
+		};
 
-        return View(cartObj);
-    }
+		return View(cartObj);
+	}
 
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    [Authorize]
-    public IActionResult Details(ShoppingCart shoppingCart)
-    {
-        var claimsIdentity = (ClaimsIdentity)User.Identity;
-        var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-        shoppingCart.ApplicationUserId = claim.Value;
+	[HttpPost]
+	[ValidateAntiForgeryToken]
+	[Authorize]
+	public IActionResult Details(ShoppingCart shoppingCart)
+	{
+		var claimsIdentity = (ClaimsIdentity)User.Identity;
+		var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+		shoppingCart.ApplicationUserId = claim.Value;
 
-        ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(
-            u => u.ApplicationUserId == claim.Value && u.ProductId == shoppingCart.ProductId);
-
-
-        if (cartFromDb == null)
-        {
-            _unitOfWork.ShoppingCart.Add(shoppingCart);
-/*            _unitOfWork.Save();
-            HttpContext.Session.SetInt32(SD.SessionCart,
-                _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count);*/
-        }
-        else
-        {
-            _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, shoppingCart.Count);
-            _unitOfWork.Save();
-        }
+		ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(
+			u => u.ApplicationUserId == claim.Value && u.ProductId == shoppingCart.ProductId);
 
 
-        return RedirectToAction(nameof(Index));
-    }
+		if (cartFromDb == null)
+		{
+			_unitOfWork.ShoppingCart.Add(shoppingCart);
+			/*            _unitOfWork.Save();
+						HttpContext.Session.SetInt32(SD.SessionCart,
+							_unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count);*/
+		}
+		else
+		{
+			_unitOfWork.ShoppingCart.IncrementCount(cartFromDb, shoppingCart.Count);
+			_unitOfWork.Save();
+		}
 
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+		return RedirectToAction(nameof(Index));
+	}
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 }
